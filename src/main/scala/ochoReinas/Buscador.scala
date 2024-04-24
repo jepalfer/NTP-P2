0f866d62d8aff461b9f8ec70ac391caa18f3ae8f
jepalfer <101323359+jepalfer@users.noreply.github.com>
2024-04-24 18:28:13 +0200

    Aproximación inicial a la solución

--- src/main/scala/ochoReinas/Buscador.scala
+++ src/main/scala/ochoReinas/Buscador.scala
@@ -3,10 +3,32 @@ package ochoReinas
 import scala.annotation.tailrec
 
 class Buscador (val dimension : Int) {
+  var contadorTableros = 0
    /**
     * metodo para ubicar nueva reina
     * @param fila
     * @return
     */
-   def resolver : Tablero = ???
+
+   def resolver : Tablero =
+      def go (fila: Int, columna: Int, contador: Int = 0, tablero: Tablero): Tablero =
+         if (contador == tablero.dimension) tablero
+         else {
+           if (!Conflicto.conflictoCeldaTablero(new Celda(fila, columna), tablero) &&
+                fila < tablero.dimension && columna < tablero.dimension)
+             println("Nueva reina colocada en " + fila + " - " + columna)
+             go(fila + 1, 0, contador + 1, tablero.agregarReina(fila, columna))
+           else {
+              if (columna == tablero.dimension - 1)
+                contadorTableros += 1
+                go(0, contadorTableros, 0, new Tablero(dimension, List()))
+              else go(fila, columna + 1, contador, tablero)
+           }
+         }
+
+      val filaInicial = 0
+      val columnaInicial = 0
+      val contadorInicial = 0
+      go(filaInicial, columnaInicial, contadorInicial, Tablero(dimension, List()))
+      
 }
